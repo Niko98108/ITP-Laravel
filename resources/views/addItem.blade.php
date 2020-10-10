1c5424ff29d06459c9b61377543c79989a9eda41
Nisal <iamnisal123@gmail.com>
2020-10-10 23:09:53 +0530

    test demo button

--- resources/views/addItem.blade.php
+++ resources/views/addItem.blade.php
@@ -15,15 +15,21 @@
     background-image:url("../images/bg-food3.jpg" );
 }
 </style>
+<script>
+    function validateForm() {
+      var x = document.forms["inputForm"]["item_name"].value;
+      if (x == "") {
+        alert("Name must be filled out");
+        return false;
+      }
+    }
+    </script>
 
 </head>
 <body>
-    @if(session('success_message'))
-    <div class= "alert alert-success">
-        {{session('success_message')}}
-    </div>
 
-    @endif
+
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
         <div class="logo_">
       <a class="navbar-brand" href="#"> <img src="images/logo_.png" alt="logo"style="width:170px"> </a>
@@ -70,14 +76,14 @@
     </nav>
     <main role="main">
     <div class="container">
-@foreach($errors ->all() as $error)
+{{-- @foreach($errors ->all() as $error)
 <div class="alert alert-danger" role="alert">
     {{$error}}
   </div>
 
-@endforeach
+@endforeach --}}
 
-<form enctype="multipart/form-data" method="POST" action="/save">
+<form enctype="multipart/form-data" method="POST" action="/save" onsubmit="return validateForm()" name="inputForm">
     {{csrf_field()}}
         <div class="card-body" >
             <div class="align-content-md-center">
@@ -93,24 +99,24 @@
                     <div class="col-md-12">
                         <div class="form-group" >
                             <label for="exampleFormControlInput1">1.Item Name</label>
-                        <input type="text" class="form-control" name="item_name" placeholder="Name">
+                        <input type="text" class="form-control" name="item_name" placeholder="Name" id="input_f1">
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlInput2">2.Item Code</label>
-                        <input type="text" class="form-control" name="item_code" placeholder="code">
+                        <input type="text" class="form-control" name="item_code" placeholder="code" id="input_f2">
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlInput3">3.Item price</label>
-                        <input type="number" class="form-control" name="item_price" placeholder="price">
+                        <input type="number" class="form-control" name="item_price" placeholder="price" id="input_f3">
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlInput4">4.Item Details</label>
-                        <input type="text" class="form-control" name="item_details" placeholder="Details">
+                        <input type="text" class="form-control" name="item_details" placeholder="Details" id="input_f4">
 
 
                         <div class="form-group">
                                 <label for="exampleFormControlInput5">5.Add Image</label><br>
-                                <input data-preview="#preview" name="input_img" type="file" id="imageInput">
+                                <input data-preview="#preview" name="input_img" type="file" id="imageInput" >
                                 <img class="col-sm-6" id="preview"  src="">
                             </div>
 
@@ -125,15 +131,25 @@
                         </div>
 
 
+                    </form>
 
-
-    </form>
  </form>
 
 
 
 
     </main>
-    @include('sweetalert::alert')
+    {{-- @include('sweetalert::alert') --}}
+    <button onclick="myFunction()" class="btn btn-outline-info">Demo</button>
+    <script>
+       function myFunction() {
+       document.getElementById("input_f1").value ="Pasta" ;
+       document.getElementById("input_f2").value = "y456" ;
+       document.getElementById("input_f3").value = 500 ;
+       document.getElementById("input_f4").value = "cheesy" ;
+    //    document.getElementById("imageInput").src = "E:/Laravel/backup/ITP/public/images/menu-sample-pasta.jpg" ;
+
+       }
+       </script>
     </body>
 </html>
